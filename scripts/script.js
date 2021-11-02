55b329b92344e53d82a4dcbedefaaff56520a1dc
Dmitriy Movchan <heavysky@yandex.ru>
2021-11-02 11:16:31 +0300

    спринт 5: доработки после 1 ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
-const body = document.querySelector('.body');
-const popup = document.querySelector('.popup');
-const popupEditButton = document.querySelector('.popup_edit-button')
+const profilePopupElement = document.querySelector('.popup_edit-profile')
 const editButton = document.querySelector('.profile__edit-button');
 const popupAddElement = document.querySelector('.popup_add-element');
 const addButton = document.querySelector('.profile__add-button');
-const popupCloseButton = document.querySelectorAll('.popup__close');
-const form = document.querySelectorAll('.popup__form');
+const popupCloseEditProfile = document.querySelector('.popup__close_edit-profile');
+const popupCloseAddElement = document.querySelector('.popup__close_add-element');
+const popupCloseBigImage = document.querySelector('.popup__close_big-image');
+const popupFormEditProfile = document.querySelector('.popup__form');
+const popupFormAddElement = document.querySelector('.popup__form_add-element');
 const popupName = document.querySelector('.popup__input_type_name');
 const profileName = document.querySelector('.profile__name');
 const popupProfession = document.querySelector('.popup__input_type_profession');
@@ -15,51 +16,8 @@ const popupInputTypeLink = document.querySelector('.popup__input_type_link');
 const popupImageBig = document.querySelector('.popup_big');
 const popupImage = document.querySelector('.popup__image');
 const popupTitle = document.querySelector('.popup__title');
-
-
-function openPopup(popup) { //открывает попап
-    popup.classList.add('popup_open') // добавление класса
-    popupName.value = profileName.textContent;
-    popupProfession.value = profileProfession.textContent;
-}
-
-function closePopup(popup) { // закрывает попап
-    popup.classList.remove('popup_open') // удаляет класс
-}
-
-editButton.addEventListener('click', () => openPopup(popupEditButton)); //слушатель нажатия на кнопу изменения профайла
-addButton.addEventListener('click', () => openPopup(popupAddElement)); //слушатель нажатия на кнопу изменения профайла
-
-popupCloseButton[0].addEventListener('click', () => closePopup(popupEditButton)); // слушатель при нажатии на крестик закрывает форму
-popupCloseButton[1].addEventListener('click', () => closePopup(popupAddElement)); // слушатель при нажатии на крестик закрывает форму
-
-
-function submitForm(event) { // 
-    event.preventDefault(); // не дает отправить данные на сервер
-    profileName.textContent = popupName.value; // меняет значение имени
-    profileProfession.textContent = popupProfession.value; //меняет значение фамилии
-    closePopup(popupEditButton); // закрывает попап при введении
-}
-
-function submitAddElement(event) { // ф-я добавления карточек
-    event.preventDefault();
-    const title = popupInputTypeTitle.value; // меняем значение title на введеное в карточке
-    const link = popupInputTypeLink.value; // меняем значение Link на введеное в карточке
-    const item = {
-        name: title,
-        link: link
-    }
-    event.target.reset();
-    prependCard(item); // добавляем карточку в начало
-    closePopup(popupAddElement); // закрываем попап добавления
-}
-window.addEventListener('load', () => { // ф-я плавного открытия/закрытия попапа
-    document.querySelectorAll('.popup').forEach((popup) => popup.classList.add('popup_transition'))
-})
-
-form[0].addEventListener('submit', submitForm) //слушает введение данных в форму изменения профайла
-form[1].addEventListener('submit', submitAddElement) //слушает введение данных в форму добавления карточек
-
+const elements = document.querySelector('.elements'); //выбирает ближайший к темплейту элемент
+const template = document.querySelector('.template').content; // берет контент темплейта
 const initialCards = [{ // массив карточек "из коробки"
         name: 'Архыз',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -85,8 +43,53 @@ const initialCards = [{ // массив карточек "из коробки"
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
-const elements = document.querySelector('.elements'); //выбирает ближайший к темплейту элемент
-const template = document.querySelector('.template').content; // берет контент темплейта
+
+function openPopup(popup) { //открывает попап
+    popup.classList.add('popup_open') // добавление класса
+
+}
+
+function closePopup(popup) { // закрывает попап
+    popup.classList.remove('popup_open') // удаляет класс
+}
+
+editButton.addEventListener('click', () => openPopup(profilePopupElement)); //слушатель нажатия на кнопу изменения профайла
+popupName.value = profileName.textContent;
+popupProfession.value = profileProfession.textContent;
+
+addButton.addEventListener('click', () => openPopup(popupAddElement)); //слушатель нажатия на кнопу изменения профайла
+
+popupCloseEditProfile.addEventListener('click', () => closePopup(profilePopupElement)); // слушатель при нажатии на крестик закрывает форму
+popupCloseAddElement.addEventListener('click', () => closePopup(popupAddElement)); // слушатель при нажатии на крестик закрывает форму
+
+
+function submitProfileForm(event) { // 
+    event.preventDefault(); // не дает отправить данные на сервер
+    profileName.textContent = popupName.value; // меняет значение имени
+    profileProfession.textContent = popupProfession.value; //меняет значение фамилии
+    closePopup(profilePopupElement); // закрывает попап при введении
+}
+
+function submitAddElement(event) { // ф-я добавления карточек
+    event.preventDefault();
+    const title = popupInputTypeTitle.value; // меняем значение title на введеное в карточке
+    const link = popupInputTypeLink.value; // меняем значение Link на введеное в карточке
+    const item = {
+        name: title,
+        link: link
+    }
+    event.target.reset();
+    prependCard(item); // добавляем карточку в начало
+    closePopup(popupAddElement); // закрываем попап добавления
+}
+window.addEventListener('load', () => { // ф-я плавного открытия/закрытия попапа
+    document.querySelectorAll('.popup').forEach((popup) => popup.classList.add('popup_transition'))
+})
+
+popupFormEditProfile.addEventListener('submit', submitProfileForm) //слушает введение данных в форму изменения профайла
+popupFormAddElement.addEventListener('submit', submitAddElement) //слушает введение данных в форму добавления карточек
+
+
 
 
 initialCards.forEach(appendCards); //обработка массива
@@ -105,7 +108,7 @@ function createCard(item) { //ф-я переработчик массива
     element.querySelector('.element__image').addEventListener('click', (event) => { // слушаем клик по картинке элемента
         popupImage.src = event.target.src; // берем ссылку на этот элемент из массива
         openPopup(popupImageBig); // открываем попап с большой картинкой
-        popupCloseButton[2].addEventListener('click', () => closePopup(popupImageBig)); // слушатель при нажатии на крестик закрывает форму
+        popupCloseBigImage.addEventListener('click', () => closePopup(popupImageBig)); // слушатель при нажатии на крестик закрывает форму
     });
 
     return element; //возвращаем блок элемента
